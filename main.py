b35be7771afad6d339236a5b614bd02d5346fc72
Igor Vonsoski <igorvonsoski@outlook.com.br>
2026-03-28 14:13:37 -0300

    inclusão de novas classes

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import modulos as consultas
 #dados cidade, no country_code, precisa estar de acordo com a ISO 3166-2BR
-city_name = 'Sergipe'
-country_code = 'BR-SE'
+city_name = 'Aracaju'
+state_code = 'SE'
+country_code = 'BR'
 
-#Chama class
+#Cria o objeto
 consulta_api = consultas.WeatherAPI()
-consulta = consulta_api.consultaAPI(city_name,country_code)
+#Chama classe para consultar e obter o json
+consulta = consulta_api.consulta_city(city_name, state_code, country_code)
+
+#Obtem as coordernadas e os valores
+lat, lon = consulta_api.obter_coord(consulta)
+
+#Envia latitude e longitude para obter os dados como temperatura e afins
+consulta_city = consulta_api.info_tempo(lat, lon)
+
+#Consulta a temperatura
+graus = consulta_api.consulta_temperatura(consulta_city)
+
+print(graus)
--- src/WeatherAPI.py
+++ src/WeatherAPI.py
@@ -3,16 +3,33 @@ import json
 from src.config import API_KEY
 
 class WeatherAPI:
-    def consultaAPI (self,city_name,country_code):
+    def consulta_city (self,city_name,state_code,country_code):
         
         params = {
-            'q':[city_name,country_code],
-            'appid':{API_KEY}
-                }
-
+            'q': f"{city_name},{state_code},{country_code}",
+            'appid': API_KEY
+        }
         url = 'https://api.openweathermap.org/geo/1.0/direct?'
         response = requests.get(url, params = params)
         return json.loads(response.text)
-
+    
     def obter_coord(self, response):
-        print(response)
+        lat = response[0]['lat']
+        lon = response[0]['lon']
+        return lat, lon
+    
+    def info_tempo (self, lat, lon):
+        params = {
+            'lat': lat,
+            'lon': lon,
+            'appid': API_KEY
+        }
+        url = 'https://api.openweathermap.org/data/2.5/weather?'
+        self.response = requests.get(url, params = params)
+        return json.loads(self.response.text)
+    
+    def consulta_temperatura (self, response):
+        temperatura = response['main']['temp']
+        weather = round(temperatura - 273.15)
+        return weather
+    
